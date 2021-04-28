af0ce753a50b9eebe657ca9dcdead5473c75c5d9
Yiying Jie <yiying.j@outlook.com>
2021-04-27 19:29:46 -0700

    example on basics of components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>Hello World</h1>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>{{ msg }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // assign a name to component for easier debugging
+  name: "App", 
+  data() {
+    return {
+      msg: "Hello World!",
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
